abc7a7dc7e536b19c1d750dc862a0880a032e29e
Amr Ahmed <aemaddin@gmail.com>
2019-11-27 23:30:46 +0200

    edited KnetServiceProvider

--- src/Providers/KnetServiceProvider.php
+++ src/Providers/KnetServiceProvider.php
@@ -18,9 +18,9 @@ class KnetServiceProvider extends ServiceProvider
          *
          * Uncomment this function call to make the config file publishable using the 'config' tag.
          */
-        // $this->publishes([
-        //     __DIR__.'/../../config/knet.php' => config_path('knet.php'),
-        // ], 'config');
+         $this->publishes([
+             __DIR__.'/../../config/knet.php' => config_path('knet.php'),
+         ], 'config');
 
         /**
          * Routes
@@ -28,7 +28,7 @@ class KnetServiceProvider extends ServiceProvider
          * Uncomment this function call to load the route files.
          * A web.php file has already been generated.
          */
-        // $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
+         $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
 
         /**
          * Translations
@@ -81,10 +81,10 @@ class KnetServiceProvider extends ServiceProvider
          * Uncomment the first function call to load the migrations.
          * Uncomment the second function call to make the migrations publishable using the 'migrations' tags.
          */
-        // $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
-        // $this->publishes([
-        //     __DIR__.'/../../database/migrations/' => database_path('migrations')
-        // ], 'migrations');
+         $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+         $this->publishes([
+             __DIR__.'/../../database/migrations/' => database_path('migrations')
+         ], 'migrations');
     }
 
     /**
@@ -100,8 +100,8 @@ class KnetServiceProvider extends ServiceProvider
          * Uncomment this function call to load the config file.
          * If the config file is also publishable, it will merge with that file
          */
-        // $this->mergeConfigFrom(
-        //     __DIR__.'/../../config/knet.php', 'knet'
-        // );
+         $this->mergeConfigFrom(
+             __DIR__.'/../../config/knet.php', 'knet'
+         );
     }
 }
