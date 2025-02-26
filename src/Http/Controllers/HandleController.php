dbce8f078e68adbad965dbe90ed5b0b8bd79c1c6
aemaddin <aemaddin@gmail.com>
2025-02-26 21:40:40 +0200

    WIP

--- src/Http/Controllers/HandleController.php
+++ src/Http/Controllers/HandleController.php
@@ -2,6 +2,8 @@
 
 namespace Asciisd\Knet\Http\Controllers;
 
+use Asciisd\Knet\Events\KnetResponseReceived;
+use Asciisd\Knet\Services\KnetResponseService;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
@@ -9,6 +11,15 @@ class HandleController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return redirect(config('knet.redirect_url'));
+        // Decrypt and parse response
+        $payloadArray = KnetResponseService::decryptAndParse($request);
+
+        logger()->info('ResponseController | Knet Response: ', $payloadArray);
+        logger()->info('ResponseController | Knet Header: ', $request->header());
+
+        // Dispatch received event
+        KnetResponseReceived::dispatch($payloadArray);
+
+        echo 'REDIRECT='.route('knet.handle');
     }
 }
